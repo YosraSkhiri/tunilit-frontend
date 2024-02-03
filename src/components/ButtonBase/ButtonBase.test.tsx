6fb449d3ca0cc8d71e8843dad8545fb76d9eed1f
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-02-03 22:26:00 +0100

    Add a tests for ButtonBase component

---
+++ src/components/ButtonBase/ButtonBase.test.tsx
@@ -0,0 +1,49 @@
+import { fireEvent, render } from '@testing-library/react'
+import { describe, expect, vi } from 'vitest'
+import ButtonBase from './ButtonBase'
+
+describe('<ButtonBase />', () => {
+	it('should render a button with type button', () => {
+		const { container } = render(<ButtonBase></ButtonBase>)
+		expect(container.firstChild?.nodeName).equal('BUTTON')
+		expect(container.firstChild).toHaveAttribute('type', 'button')
+	})
+
+	it('should change the button type', () => {
+    const { container } = render(<ButtonBase type='submit'></ButtonBase>)
+    expect(container.firstChild).toHaveAttribute('type', 'submit')
+  })
+
+	it('should automatically change the button to an anchor element when href is provided', () => {
+    const { container } = render(<ButtonBase href='#'></ButtonBase>)
+    expect(container.firstChild?.nodeName).equal('A')
+  })
+
+  describe('event callbacks', () => {
+    it('should fire event callbacks', () => {
+      const onClick = vi.fn()
+      const onBlur = vi.fn()
+      const onFocus = vi.fn()
+
+      const { container } = render(
+        <ButtonBase
+          onClick={onClick}
+          onBlur={onBlur}
+          onFocus={onFocus}
+        >
+        </ButtonBase>
+      )
+
+      const button = container.firstChild
+
+      button && fireEvent.click(button)
+      expect(onClick).toHaveBeenCalledTimes(1)
+
+      button && fireEvent.focus(button)
+      expect(onFocus).toHaveBeenCalledTimes(1)
+
+      button && fireEvent.blur(button)
+      expect(onFocus).toHaveBeenCalledTimes(1)
+    })
+  })
+})
