edf3d14eff4b4c5f4b451758e58dc0302832a2f8
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-02-09 21:35:11 +0100

    lint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,6 @@
 import { Fragment, useRef } from 'react'
 import ButtonBase from '../ButtonBase'
-import {
-  LoaderIcon,
-} from '../Icons'
+import { LoaderIcon } from '../Icons'
 import ButtonProps from './Button.types'
 import styles from './Button.module.scss'
 import classNames from 'classnames'
@@ -27,8 +25,10 @@ const Button = ({
 			[styles[`btn--${variant}`]]: !disabled && !loading,
 			[styles[`btn--${size}`]]: size,
 			[styles['btn--full-width']]: fullWidth,
-      [styles['btn--disabled-outlined']]: disabled || loading && variant !== "subtle",
-			[styles['btn--disabled-subtle']]: disabled || loading && variant === "subtle",
+			[styles['btn--disabled-outlined']]:
+				(disabled || loading) && variant !== 'subtle',
+			[styles['btn--disabled-subtle']]:
+				(disabled || loading) && variant === 'subtle',
 		},
 		className
 	)
@@ -43,8 +43,7 @@ const Button = ({
 
 	const arrowSvgRef = useRef<SVGSVGElement>(null)
 
-	const WrapperElem: WrapperElemType =
-		(shadow || arrow) ? 'div' : Fragment
+	const WrapperElem: WrapperElemType = shadow || arrow ? 'div' : Fragment
 
 	const handleFocus = (e?: React.MouseEvent<HTMLElement>) => {
 		if (onFocus) onFocus(e)
@@ -77,48 +76,50 @@ const Button = ({
 				className={buttonClass}
 				disabled={disabled}
 				{...other}>
-          {
-            loading && (
-              <LoaderIcon size={size} classname={styles['loader--animation']} />
-            )
-          }
+				{loading && (
+					<LoaderIcon
+						size={size}
+						classname={styles['loader--animation']}
+					/>
+				)}
 				{loading ? 'Loading' : children}
 			</ButtonBase>
-      {
-        arrow && variant !== 'subtle' ?
-          <svg 
-            ref={arrowSvgRef}
-            className={`${styles.arrow} ${disabled ? styles['arrow--disabled'] : ''}`}
-            width="72" 
-            height="17" 
-            viewBox="0 0 72 17" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg"
-            data-testid='arrow-svg'
-            focusable={false}
-            aria-hidden={true}
-          >
-            <path 
-              className={styles.path1}
-              d="M1.16111 8.20063C7.25514 8.21308 60.5781 8.32203 66.3199 8.33377" 
-              strokeWidth="2" 
-              strokeLinecap="round"
-            />
-            <path 
-              className={styles.path2}
-              d="M58.8581 1.25788C62.2902 5.50703 66.6865 8.32578 70.7984 8.23543" 
-              strokeWidth="2" 
-              strokeLinecap="round" 
-            />
-            <path
-              className={styles.path3}
-              d="M59.265 15.7288C62.454 11.3341 66.6865 8.32578 70.7984 8.23543" 
-              strokeWidth="2" 
-              strokeLinecap="round"
-            />
-          </svg>
-          : ''
-      }
+			{arrow && variant !== 'subtle' ? (
+				<svg
+					ref={arrowSvgRef}
+					className={`${styles.arrow} ${
+						disabled ? styles['arrow--disabled'] : ''
+					}`}
+					width="72"
+					height="17"
+					viewBox="0 0 72 17"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+					data-testid="arrow-svg"
+					focusable={false}
+					aria-hidden={true}>
+					<path
+						className={styles.path1}
+						d="M1.16111 8.20063C7.25514 8.21308 60.5781 8.32203 66.3199 8.33377"
+						strokeWidth="2"
+						strokeLinecap="round"
+					/>
+					<path
+						className={styles.path2}
+						d="M58.8581 1.25788C62.2902 5.50703 66.6865 8.32578 70.7984 8.23543"
+						strokeWidth="2"
+						strokeLinecap="round"
+					/>
+					<path
+						className={styles.path3}
+						d="M59.265 15.7288C62.454 11.3341 66.6865 8.32578 70.7984 8.23543"
+						strokeWidth="2"
+						strokeLinecap="round"
+					/>
+				</svg>
+			) : (
+				''
+			)}
 		</WrapperElem>
 	)
 }
