d7636d7e63106bcfd15cf0263cd9ebb6bf0d57da
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-02-09 21:34:54 +0100

    Seperate stories for each button kind

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,164 +1,134 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Button from './Button'
-import { buttonVariantType } from './Button.types'
 import { PlaceholderIcon } from '../Icons'
-import Layout from '../Layout'
 
 const meta: Meta<typeof Button> = {
 	component: Button,
-  argTypes: {
-    component: {
-      table: {
-        disable: true,
-      },
-    },
-    onBlur: {
-      table: {
-        disable: true,
-      },
-    },
-    onClick: {
-      table: {
-        disable: true,
-      },
-    },
-    onFocus: {
-      table: {
-        disable: true,
-      },
-    },
-    className: {
-      table: {
-        disable: true,
-      }
-    }
-  }
 }
 
 export default meta
 
 type Story = StoryObj<typeof Button>
 
-const variants: buttonVariantType[] = [
-	'primary',
-	'secondary',
-	'tertiary',
-	'success',
-	'error',
-  'subtle',
-]
+export const Primary: Story = {
+	render: () => <Button>Button</Button>,
+}
 
-export const Overview: Story = {
+export const Secondary: Story = {
+	render: () => <Button variant="secondary">Button</Button>,
+}
+
+export const Tertiary: Story = {
+	render: () => <Button variant="tertiary">Button</Button>,
+}
+
+export const Success: Story = {
+	render: () => <Button variant="success">Button</Button>,
+}
+
+export const Error: Story = {
+	render: () => <Button variant="error">Button</Button>,
+}
+
+export const Subtle: Story = {
+	render: () => <Button variant="subtle">Button</Button>,
+}
+
+export const IconLeftButton: Story = {
 	render: () => (
-		<Layout style={{ gap: 80, padding: 40, minWidth: 1300 }} direction='column'>
-      <Layout style={{ gap: 60 }}>
-        <Layout
-          direction="column"
-          style={{ gap: 40 }}>
-          {variants.map((variant: buttonVariantType, i: number) => (
-            <Button variant={variant} key={`a-${i}`}>
-              <PlaceholderIcon />
-              Button
-            </Button>
-          ))}
-        </Layout>
-        <Layout
-          direction="column"
-          style={{ gap: 40 }}>
-            {variants.map((variant: buttonVariantType, i: number) => (
-            <Button variant={variant} key={`b-${i}`}>
-              Button
-              <PlaceholderIcon />
-            </Button>
-          ))}
-        </Layout>
-        <Layout
-          direction="column"
-          style={{ gap: 40 }}>
-          {variants.map((variant: buttonVariantType, i: number) => (
-            <Button variant={variant} shadow key={`c-${i}`}>
-              <PlaceholderIcon />
-              Button
-            </Button>
-          ))}
-        </Layout>
-        <Layout
-          direction="column"
-          style={{ gap: 40 }}>
-            {variants.map((variant: buttonVariantType, i: number) => (
-            <Button variant={variant} shadow key={`d-${i}`}>
-              Button
-              <PlaceholderIcon />
-            </Button>
-          ))}
-        </Layout>
-		</Layout>
-    <Layout style={{ gap: 60 }}>
-			<Layout
-				direction="column"
-				style={{ gap: 40 }}>
-				{variants.map((variant: buttonVariantType, i: number) => (
-					<Button variant={variant} size='lg' key={`e-${i}`}>
-						<PlaceholderIcon size='lg' />
-						Button
-					</Button>
-				))}
-			</Layout>
-			<Layout
-				direction="column"
-				style={{ gap: 40 }}>
-          {variants.map((variant: buttonVariantType, i: number) => (
-					<Button variant={variant} size='lg' key={`f-${i}`}>
-						Button
-            <PlaceholderIcon size='lg' />
-					</Button>
-				))}
-			</Layout>
-      <Layout
-				direction="column"
-				style={{ gap: 40 }}>
-				{variants.map((variant: buttonVariantType, i: number) => (
-					<Button variant={variant} shadow size='lg' key={`g-${i}`}>
-						<PlaceholderIcon size='lg' />
-						Button
-					</Button>
-				))}
-			</Layout>
-			<Layout
-				direction="column"
-				style={{ gap: 40 }}>
-          {variants.map((variant: buttonVariantType, i: number) => (
-					<Button variant={variant} shadow size='lg' key={`h-${i}`}>
-						Button
-            <PlaceholderIcon size='lg' />
-					</Button>
-				))}
-			</Layout>
-		</Layout>
-    <Button fullWidth>Full width button</Button>
-    <Layout style={{ gap: 80 }}>
-      <Button arrow>Button</Button>
-      <Button disabled variant='primary'>Button</Button>
-      <Button loading variant='primary'>Button</Button>
-      <Button disabled variant='subtle'>Button</Button>
-      <Button loading variant='subtle'>Button</Button>
-    </Layout>
-    <Layout style={{ gap: 80 }}>
-      <Button arrow shadow>Button</Button>
-      <Button disabled variant='primary' shadow>Button</Button>
-      <Button loading variant='primary' shadow>Button</Button>
-      <Button disabled variant='subtle' shadow>Button</Button>
-      <Button loading variant='subtle' shadow>Button</Button>
-    </Layout>
-    </Layout>
+		<Button>
+			<PlaceholderIcon />
+			Button
+		</Button>
 	),
 }
 
-export const Example: Story = {
-  args: {
-    children: 'Button',
-  },
-  render: (args) => (
-    <Button {...args} />
-  )
+export const IconRightButton: Story = {
+	render: () => (
+		<Button>
+			Button
+			<PlaceholderIcon />
+		</Button>
+	),
+}
+
+export const ShadowButton: Story = {
+	render: () => <Button shadow>Button</Button>,
+}
+
+export const PrimaryArrowButton: Story = {
+	render: () => <Button arrow>Button</Button>,
+}
+
+export const MediumButton: Story = {
+	render: () => <Button>Button</Button>,
+}
+
+export const LargeButton: Story = {
+	render: () => <Button size="lg">Button</Button>,
+}
+
+export const DisabledOutlinedButton: Story = {
+	render: () => <Button disabled>Button</Button>,
+}
+
+export const DisabledSubtleButton: Story = {
+	render: () => (
+		<Button
+			variant="subtle"
+			disabled>
+			Button
+		</Button>
+	),
+}
+
+export const LoadingOutlinedButton: Story = {
+	render: () => <Button loading>Button</Button>,
+}
+
+export const LoadingSubtleButton: Story = {
+	render: () => (
+		<Button
+			variant="subtle"
+			loading>
+			Button
+		</Button>
+	),
+}
+
+export const Playground: Story = {
+	parameters: {
+		layout: 'centered',
+	},
+	args: {
+		children: 'Button',
+	},
+	argTypes: {
+		component: {
+			table: {
+				disable: true,
+			},
+		},
+		onBlur: {
+			table: {
+				disable: true,
+			},
+		},
+		onClick: {
+			table: {
+				disable: true,
+			},
+		},
+		onFocus: {
+			table: {
+				disable: true,
+			},
+		},
+		className: {
+			table: {
+				disable: true,
+			},
+		},
+	},
+	render: (args) => <Button {...args} />,
 }
