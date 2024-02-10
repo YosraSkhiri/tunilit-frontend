b6c03f3596bebdfd75ff301a180598e90818db89
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-02-10 18:58:26 +0100

    Make seperate story for each button variant

--- src/components/IconButton/IconButton.stories.tsx
+++ src/components/IconButton/IconButton.stories.tsx
@@ -1,79 +1,149 @@
 import { Meta, StoryObj } from '@storybook/react'
 import IconButton from './IconButton'
 import { PlaceholderIcon } from '../Icons'
-import Layout from '../Layout'
-import { buttonVariantType } from './IconButton.type'
 
 const meta: Meta<typeof IconButton> = {
-  component: IconButton,
+	component: IconButton,
 }
 
 export default meta
 
 type Story = StoryObj<typeof IconButton>
 
-const variants: buttonVariantType[] = [
-	'primary',
-	'secondary',
-	'tertiary',
-	'success',
-	'error',
-  'subtle',
-]
-
-export const Overview: Story = {
-  render: () => (
-    <Layout direction='column' style={{gap: 60}}>
-      <Layout style={{gap: 40}}>
-        {
-          variants.map((variant : buttonVariantType) => (
-            <IconButton variant={variant}>
-              <PlaceholderIcon />
-            </IconButton>
-          ))
-        }
-        <IconButton disabled>
-          <PlaceholderIcon />
-        </IconButton>
-        <IconButton variant='subtle' disabled>
-          <PlaceholderIcon />
-        </IconButton>
-        <IconButton loading>
-          <PlaceholderIcon />
-        </IconButton>
-        <IconButton variant='subtle' loading>
-          <PlaceholderIcon />
-        </IconButton>
-      </Layout>
-      <Layout style={{gap: 40}}>
-        {
-          variants.map((variant : buttonVariantType) => (
-            <IconButton size='lg' variant={variant}>
-              <PlaceholderIcon size='lg' />
-            </IconButton>
-          ))
-        }
-        <IconButton disabled size='lg'>
-          <PlaceholderIcon size='lg' />
-        </IconButton>
-        <IconButton variant='subtle' disabled size='lg'>
-          <PlaceholderIcon size='lg' />
-        </IconButton>
-        <IconButton loading size='lg'>
-          <PlaceholderIcon size='lg' />
-        </IconButton>
-        <IconButton variant='subtle' loading size='lg'>
-          <PlaceholderIcon size='lg' />
-        </IconButton>
-      </Layout>
-    </Layout>
-  )
+export const Primary: Story = {
+	render: () => (
+		<IconButton>
+			<PlaceholderIcon />
+		</IconButton>
+	),
 }
 
-export const Example: Story = {
-  render: (args) => (
-    <IconButton {...args}>
-      <PlaceholderIcon />
-    </IconButton>
-  )
+export const Secondary: Story = {
+	render: () => (
+		<IconButton variant="secondary">
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const Tertiary: Story = {
+	render: () => (
+		<IconButton variant="tertiary">
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const Success: Story = {
+	render: () => (
+		<IconButton variant="success">
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const Error: Story = {
+	render: () => (
+		<IconButton variant="error">
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const Subtle: Story = {
+	render: () => (
+		<IconButton variant="subtle">
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const MediumButton: Story = {
+	render: () => (
+		<IconButton>
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const LargeButton: Story = {
+	render: () => (
+		<IconButton size="lg">
+			<PlaceholderIcon size="lg" />
+		</IconButton>
+	),
+}
+
+export const DisabledOutlinedButton: Story = {
+	render: () => (
+		<IconButton disabled>
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const DisabledSubtleButton: Story = {
+	render: () => (
+		<IconButton
+			variant="subtle"
+			disabled>
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const LoadingOutlinedButton: Story = {
+	render: () => (
+		<IconButton loading>
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const LoadingSubtleButton: Story = {
+	render: () => (
+		<IconButton
+			variant="subtle"
+			loading>
+			<PlaceholderIcon />
+		</IconButton>
+	),
+}
+
+export const Playground: Story = {
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		component: {
+			table: {
+				disable: true,
+			},
+		},
+		onBlur: {
+			table: {
+				disable: true,
+			},
+		},
+		onClick: {
+			table: {
+				disable: true,
+			},
+		},
+		onFocus: {
+			table: {
+				disable: true,
+			},
+		},
+		className: {
+			table: {
+				disable: true,
+			},
+		},
+	},
+	render: (args) => (
+		<IconButton {...args}>
+			<PlaceholderIcon />
+		</IconButton>
+	),
 }
