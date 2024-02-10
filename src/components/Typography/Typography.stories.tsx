ab45204685d063e39793eb86369339b34a49d76f
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-02-10 17:14:31 +0100

    Make a story for each Typography variant

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -1,35 +1,113 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import Typography from './Typography'
-import Layout from '../Layout'
 
 const meta: Meta<typeof Typography> = {
-  component: Typography,
+	component: Typography,
 }
 
 export default meta
 
 type Story = StoryObj<typeof Typography>
 
-export const Overview: Story = {
-  render: () => <Layout direction='column' style={{ gap: 40, padding: 40 }}>
-    <Typography variant='title'>Title</Typography>
-    <Typography variant='h1'>Heading 1</Typography>
-    <Typography variant='h2'>Heading 2</Typography>
-    <Typography variant='h3'>Heading 3</Typography>
-    <Typography variant='h4'>Heading 4</Typography>
-    <Typography variant='h5'>Heading 5</Typography>
-    <Typography variant='h6'>Heading 6</Typography>
-    <Typography variant='subtitle1'>Subtitle 1</Typography>
-    <Typography variant='subtitle2'>Subtitle 2</Typography>
-    <Typography variant='body1'>Body 1</Typography>
-    <Typography variant='body2'>Body 2</Typography>
-    <Typography variant='body3'>Body 3</Typography>
-  </Layout>
+export const Title: Story = {
+	render: () => (
+		<Typography variant="title">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
 }
 
-export const Example: Story = {
-  args: {
-    children: 'Typography',
-  },
-  render: (args) => <Typography {...args} />
+export const Heading1: Story = {
+	render: () => (
+		<Typography variant="h1">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Heading2: Story = {
+	render: () => (
+		<Typography variant="h2">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Heading3: Story = {
+	render: () => (
+		<Typography variant="h3">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Heading4: Story = {
+	render: () => (
+		<Typography variant="h4">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Heading5: Story = {
+	render: () => (
+		<Typography variant="h5">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Heading6: Story = {
+	render: () => (
+		<Typography variant="h6">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Subtitle1: Story = {
+	render: () => (
+		<Typography variant="subtitle1">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Subtitle2: Story = {
+	render: () => (
+		<Typography variant="subtitle2">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Body1: Story = {
+	render: () => (
+		<Typography variant="body1">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Body2: Story = {
+	render: () => (
+		<Typography variant="body2">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Body3: Story = {
+	render: () => (
+		<Typography variant="body3">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit
+		</Typography>
+	),
+}
+
+export const Playground: Story = {
+	args: {
+		children: 'Typography',
+	},
+	render: (args) => <Typography {...args} />,
 }
