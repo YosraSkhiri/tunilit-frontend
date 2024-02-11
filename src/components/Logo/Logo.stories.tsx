ca0d820c37406586ec84c9070cb99b84ebade8aa
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-02-11 17:16:47 +0100

    Edit Logo stories

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,35 +1,71 @@
 import { Meta, StoryObj } from '@storybook/react'
 import Logo from './Logo'
-import Layout from '../Layout'
 
 const meta: Meta<typeof Logo> = {
-  component: Logo,
+	component: Logo,
 }
 
 export default meta
 
 type Story = StoryObj<typeof Logo>
 
-export const Overview: Story = {
-  render: () => (
-    <Layout direction='column' style={{ gap: 100 }}>
-      <Layout direction='column' style={{ gap: 60 }}>
-        <Logo animate iconWidth={316.96} iconHeight={275.01} textWidth={544.03} />
-        <div style={{ backgroundColor: '#046862'}}>
-          <Logo lightText iconWidth={316.96} iconHeight={275.01} textWidth={544.03} />
-        </div>
-      </Layout>
-      <Layout flexWrap='wrap' style={{ gap: 60 }}>
-        <Logo onlyIcon eyes='open' iconWidth={260} iconHeight={260} />
-        <Logo onlyIcon eyes='closed' iconWidth={260} iconHeight={260} />
-        <Logo onlyIcon eyes='wink' iconWidth={260} iconHeight={260} />
-      </Layout>
-    </Layout>
-  )
+export const FullLogo: Story = {
+	render: () => (
+		<Logo
+			animate
+			iconWidth={316.96}
+			iconHeight={275.01}
+			textWidth={544.03}
+		/>
+	),
 }
 
-export const Example: Story = {
-  render: (args) => (
-    <Logo {...args} />
-  )
+export const FullLogoOnDarkBackground: Story = {
+	render: () => (
+		<div style={{ backgroundColor: '#046862' }}>
+			<Logo
+				lightText
+				iconWidth={316.96}
+				iconHeight={275.01}
+				textWidth={544.03}
+			/>
+		</div>
+	),
+}
+
+export const OpenEyesIcon: Story = {
+	render: () => (
+		<Logo
+			onlyIcon
+			eyes="open"
+			iconWidth={260}
+			iconHeight={260}
+		/>
+	),
+}
+
+export const ClosedEyesIcon: Story = {
+	render: () => (
+		<Logo
+			onlyIcon
+			eyes="closed"
+			iconWidth={260}
+			iconHeight={260}
+		/>
+	),
+}
+
+export const WinkIcon: Story = {
+	render: () => (
+		<Logo
+			onlyIcon
+			eyes="wink"
+			iconWidth={260}
+			iconHeight={260}
+		/>
+	),
+}
+
+export const Playground: Story = {
+	render: (args) => <Logo {...args} />,
 }
