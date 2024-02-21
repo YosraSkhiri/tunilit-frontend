c50e037ed69f2949c72e5280d044a8886d4dfd7a
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-02-21 05:36:14 +0100

    Add ToastContainer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './sass/react-toastify/main.scss'
+import { Slide, ToastContainer } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+		<ToastContainer
+			limit={1}
+			autoClose={false}
+			hideProgressBar={true}
+      position='bottom-left'
+      closeButton={false}
+      transition={Slide}
+		/>
+	</React.StrictMode>
 )
