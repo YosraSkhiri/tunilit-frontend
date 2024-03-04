b8bc8bbe9f9581581a3d14628b0b45e57e24f195
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-03-04 09:30:13 +0100

    Extend ButtonBaseProps

--- src/components/InputButton/InputButton.types.ts
+++ src/components/InputButton/InputButton.types.ts
@@ -1,9 +1,11 @@
 import { ReactNode } from 'react'
 
+import ButtonBaseProps from '../ButtonBase/ButtonBase.types'
+
 type variantType = 'default' | 'info' | 'success' | 'error'
 type sizeType = 'md' | 'lg'
 
-export default interface InputButtonProps {
+export default interface InputButtonProps extends ButtonBaseProps {
 	ariaLabel?: string,
   children: ReactNode,
   disabled?: boolean,
