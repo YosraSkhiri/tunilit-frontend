9555068ec4adb5f0d2115ccbff65e03c2d72cd79
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-03-04 09:02:51 +0100

    Add missing props and fix types

--- src/components/ButtonBase/ButtonBase.types.ts
+++ src/components/ButtonBase/ButtonBase.types.ts
@@ -1,16 +1,18 @@
-import { ElementType, KeyboardEvent,MouseEvent } from 'react'
+import { ElementType, FocusEvent,KeyboardEvent,MouseEvent } from 'react'
 
 type BtnTypes = 'button' | 'submit' | 'reset'
 
-interface ButtonBaseProps {
+interface ButtonBaseProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  active?: string,
   children?: React.ReactNode,
   className?: string,
   component?: string | ElementType,
+  ['data-value']?: string,
   disabled?: boolean,
   href?: string,
-  onBlur?: (e?: MouseEvent<HTMLElement>) => void,
-  onClick?: (e?: MouseEvent<HTMLElement>) => void,
-  onFocus?: (e?: MouseEvent<HTMLElement>) => void,
+  onBlur?: (e?: FocusEvent<HTMLButtonElement | HTMLAnchorElement>) => void,
+  onClick?: (e?: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void,
+  onFocus?: (e?: FocusEvent<HTMLButtonElement | HTMLAnchorElement>) => void,
   onKeyDown?: (e?: KeyboardEvent) => void,
   role?: string,
   tabIndex?: number,
