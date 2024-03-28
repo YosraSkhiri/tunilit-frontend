221a3cc94538dde01dfcae3856e04e996fe54b63
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-03-28 01:44:27 +0100

    Delete

--- src/components/Menu/Menu.types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement, ReactNode } from 'react'
-
-interface MenuProps {
-	children?: ReactNode
-	cornerRadius?: 0 | 10 | 20,
-  renderMenuTrigger: (props: object) => ReactElement
-}
-
-export default MenuProps
--- src/components/MenuContext/index.tsx
+++
@@ -1,3 +0,0 @@
-import MenuContext from './MenuContext'
-
-export default MenuContext
