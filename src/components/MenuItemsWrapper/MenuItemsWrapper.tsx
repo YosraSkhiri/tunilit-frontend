a132a05a623121a9498986db956ffa9814b530f2
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-05-05 12:02:13 +0100

    Fix the padding

--- src/components/MenuItemsWrapper/MenuItemsWrapper.tsx
+++ src/components/MenuItemsWrapper/MenuItemsWrapper.tsx
@@ -1,8 +1,11 @@
 import clsx from 'clsx'
-import { forwardRef } from 'react'
+import { forwardRef, useEffect, useRef } from 'react'
 
+import { mergeRefs } from '../../utils'
 import styles from './MenuItemsWrapper.module.scss'
 import MenuItemsWrapperProps from './MenuItemsWrapper.types'
+
+let didRunOnce = false;
 
 const MenuItemsWrapper = forwardRef<HTMLDivElement, MenuItemsWrapperProps>(({
   children,
@@ -12,19 +15,47 @@ const MenuItemsWrapper = forwardRef<HTMLDivElement, MenuItemsWrapperProps>(({
   maxWidth = true,
   ...other
 }, ref) => {
+  const menuRef = useRef<HTMLDivElement>(null)
+  const menuWrappperRef = useRef<HTMLDivElement>(null)
+  
   const menuWrapperClass = clsx({
     [styles['menu-wrapper']]: true,
     [styles['menu-wrapper--maxWidth']]: maxWidth,
     [styles['menu-wrapper--fullWidth']]: fullWidth,
     [styles[`menu-wrapper--${cornerRadius}`]]: cornerRadius,
   }, className)
+
+  const menuClass = clsx({
+    [styles.menu]: true,
+  })
+
+  const updateMenuClass = () => {
+    if (menuRef.current && menuWrappperRef.current) {
+      const shouldUsePClass = menuRef.current.scrollHeight > menuRef.current.clientHeight
+      menuRef.current.classList.toggle(styles['menu--pr'], shouldUsePClass)
+      menuRef.current.classList.toggle(styles['menu-wrapper--pr'], shouldUsePClass)
+    }
+  }
+
+  useEffect(() => {
+    if (!didRunOnce) {
+      didRunOnce = true;
+      updateMenuClass()
+    }
+  }, [])
+
   return (
 	<div
 		className={menuWrapperClass}
-		ref={ref}
+    ref={mergeRefs(ref, menuWrappperRef)}
 		{...other}
     >
-		<div className={styles.menu}>{children}</div>
+		<div 
+      className={menuClass} 
+      ref={menuRef}
+    >
+      {children}
+    </div>
 	</div>
   )
 })
