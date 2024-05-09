eb4bc1faf62139bb4570fc57d086d58ebd1d034b
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-05-09 21:40:20 +0100

    Setup database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import { type Config } from "drizzle-kit";
 
-//import { env } from "~/env";
+import { env } from "./src/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL,
+    connectionString: env.POSTGRES_URL,
   },
   tablesFilter: ["tl_*"],
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,39 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import dotenvExpand from 'dotenv-expand'
+import vike from 'vike/plugin'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
 // using Vitest with Jest-DOM and React Testing Library: https://www.youtube.com/watch?v=G-4zgIPsjkU
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    css: true,
-    setupFiles: './src/test/setup.ts',
-    include: ['./src/**/*.test.tsx'],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+// https://github.com/vercel/storage/tree/main/packages/postgres#a-note-for-vite-users
+export default defineConfig(({ mode }) => {
+  // This check is important!
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '');
+    dotenvExpand.expand({ parsed: env });
+  }
+
+  return {
+    plugins: [react(), vike()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		css: true,
+		setupFiles: './src/test/setup.ts',
+		include: ['./src/**/*.test.tsx'],
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
           @use './src/sass/variables/_color-mapped.scss' as *;
           @use './src/sass/variables/_sizes-spaces.scss' as *;
         `,
-      },
-    },
-  },
-})
+			},
+		},
+	},
+  };
+});
