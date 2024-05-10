8fa0b2b261430a96b28ec7548c9c0b71cdcae20d
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-05-11 00:40:01 +0100

    Edit categories dropdown component

--- src/components/CategoriesDropdown/CategoriesDropdown.tsx
+++ src/components/CategoriesDropdown/CategoriesDropdown.tsx
@@ -1,44 +1,15 @@
 import { Popover } from '@headlessui/react'
+import { nanoid } from 'nanoid'
 
-import Button from '../Button';
-import { ChevronDownIcon, ChevronSmallRightIcon } from '../Icons';
-import Link from '../Link';
-import MenuItemContent from '../MenuItemContent';
-import Typography from '../Typography';
-import styles from './CategoriesDropdown.module.scss';
-import CategoriesDropdownProps from './CategoriesDropdown.types.ts'
-// TODO ... Complete the categories dropdown
-/**
- * Languages
- * Art
- * IT & Software
- * Business
- * Beauty & makeup
- * Design
- * Music
- * Photography & Video
- * Marketing
- */
-const data = {
-  categories: [
-    {
-      name: 'Art',
-      subCategories: [
-        {
-          name: '',
-          popularTopics: [{
-            name: '',
-            href: '',
-          }]
-        }
-      ]
-    }
-  ]
-}
+import Button from '../Button'
+import { ChevronDownIcon, ChevronSmallRightIcon } from '../Icons'
+import MenuItemContent from '../MenuItemContent'
+import MenuItemsWrapper from '../MenuItemsWrapper'
+import CategoriesDropdownProps, { categoryType }  from './CategoriesDropdown.types.ts'
 
-const CategoriesDropdown = ({}: CategoriesDropdownProps) => {
+const CategoriesDropdown = ({categories, className}: CategoriesDropdownProps) => {
   return (
-    <Popover>
+    <Popover className={className}>
       <Popover.Button as='div'>
         <Button variant='subtle'>
           Categories
@@ -47,50 +18,19 @@ const CategoriesDropdown = ({}: CategoriesDropdownProps) => {
       </Popover.Button>
 
       <Popover.Panel>
-
-          <div className={styles['menu-wrapper']}>
-          <div className={styles['menu-items-container']}>
-            <div className={styles['menu-items']}>
+        <MenuItemsWrapper cornerRadius={10} style={{position: 'absolute'}}> 
+          {
+            categories.map((cat: categoryType) => (
               <MenuItemContent 
+                key={nanoid()}
                 renderRightAdorn={(props) => <ChevronSmallRightIcon {...props} />}
                 size='sm'
               >
-                Category 1
+                {cat.name}
               </MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-            
-            </div>
-            <div className={styles['menu-items']}>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-              <MenuItemContent size='sm'>Category 1</MenuItemContent>
-            </div>
-            <div className={styles['menu-items']}>
-              <Link>Link1</Link>
-              <Link>Link1</Link>
-              <Link>Link1</Link>
-              <Link>Link1</Link>
-            </div>
-          </div>
-          </div>
-          
-          
-          
+            ))
+          } 
+        </MenuItemsWrapper>
       </Popover.Panel>
     </Popover>
   )
--- src/components/CategoriesDropdown/CategoriesDropdown.types.ts
+++ src/components/CategoriesDropdown/CategoriesDropdown.types.ts
@@ -1,3 +1,9 @@
-export default interface CategoriesDropdownProps {
+export type categoryType = {
+  link: string,
+  name: string
+}
 
+export default interface CategoriesDropdownProps {
+  categories: Array<categoryType>,
+  className: string,
 }
