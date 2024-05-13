8eb4d35b3a13f1aac3baffada007777c32de65cc
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-05-13 22:14:45 +0100

    index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,47 @@
+import Accordion from './Accordion'
+import Autocomplete from './Autocomplete'
+import Badge from './Badge'
+import BestDealBadge from './BestDealBadge'
+import Breadcrumbs from './Breadcrumbs'
+import Button from './Button'
+import ButtonBase from './ButtonBase'
+import Checkbox from './Checkbox'
+import Chip from './Chip'
+import Footer from './Footer'
+import IconButton from './IconButton'
+import Layout from './Layout'
+import Link from './Link'
+import Navbar from './Navbar'
+import Radio from './Radio'
+import SchoolCard from './SchoolCard'
+import SchoolLogo from './SchoolLogo'
+import Tabs from './Tabs'
+import Tag from './Tag'
+import TextField from './TextField'
+import Toast from './Toast'
+import Typography from './Typography'
+
+export {
+	Accordion,
+	Autocomplete,
+	Badge,
+	BestDealBadge,
+	Breadcrumbs,
+	Button,
+	ButtonBase,
+	Checkbox,
+	Chip,
+	Footer,
+	IconButton,
+	Layout,
+	Link,
+	Navbar,
+	Radio,
+	SchoolCard,
+	SchoolLogo,
+	Tabs,
+	Tag,
+	TextField,
+	Toast,
+	Typography,
+}
