22870eeef1d4a7c65a99ed8d952e9dbef943e909
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-05-18 22:48:08 +0100

    Add a loading status

--- src/components/SchoolLogo/SchoolLogo.tsx
+++ src/components/SchoolLogo/SchoolLogo.tsx
@@ -1,10 +1,14 @@
 import * as Avatar from '@radix-ui/react-avatar'
 import clsx from 'clsx'
+import { useState } from 'react'
 
+import Skeleton from '../Skeleton'
 import styles from './SchoolLogo.module.scss'
 import SchoolLogoProps from './SchoolLogo.types'
 
 const SchoolLogo = ({ active, alt, ambientMode, className, size = 'medium', src }: SchoolLogoProps) => {
+  const [loading, setLoading] = useState(true);
+
 	const logoClass = clsx({
 		[styles['s-logo']]: true,
 		[styles['s-logo--active']]: active,
@@ -14,6 +18,11 @@ const SchoolLogo = ({ active, alt, ambientMode, className, size = 'medium', src 
   const logoImgClass = clsx({
     [styles['s-logo__img']]: true,
     [styles[`s-logo--${size}-border`]]: true,
+  })
+
+  const skeletonClass = clsx({
+    [styles['s-logo__skeleton']]: true,
+    [styles[`s-logo--${size}`]]: size,
   })
 
   const logoFallbackClass = clsx({
@@ -28,7 +37,19 @@ const SchoolLogo = ({ active, alt, ambientMode, className, size = 'medium', src 
           alt={alt}
           className={logoImgClass}
           src={src}
+          onLoadingStatusChange={(status) => {
+            if (status === "loading") {
+              setLoading(true)
+            } else {
+              setLoading(false)
+            }
+          }}
         />
+        {
+          loading && (
+            <Skeleton className={skeletonClass} inline={true} />
+          )
+        }
         {
           ambientMode && (
             <Avatar.Image
@@ -38,7 +59,9 @@ const SchoolLogo = ({ active, alt, ambientMode, className, size = 'medium', src 
             />
           )
         }
-        <Avatar.Fallback className={logoFallbackClass} delayMs={600} />
+        {
+          !loading && <Avatar.Fallback className={logoFallbackClass} delayMs={600} />
+        }
       </div>
     </Avatar.Root>
 	)
