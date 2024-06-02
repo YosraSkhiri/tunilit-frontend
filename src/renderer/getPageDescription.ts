b3d8a0981aea68f2529bab7b2d98b4664cc53c3e
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-06-02 01:49:47 +0100

    Get title and description by page

--- src/renderer/getPageDescription.ts
+++ src/renderer/getPageDescription.ts
@@ -1,9 +1,9 @@
-export { getPageTitle }
+export { getPageDescription }
 
 import type { PageContext } from 'vike/types'
 
-function getPageTitle(pageContext: PageContext): string {
-  const val = pageContext.config.title
+function getPageDescription(pageContext: PageContext): string {
+  const val = pageContext.config.description
   if (typeof val === 'string') return val
   if (typeof val === 'function') return val(pageContext)
   return 'Tunilit'
