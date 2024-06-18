558ee5e4b8f6e046eec5bf2228705ad090aadb82
Yosra Skhiri <yosra.skhiri.mail@gmail.com>
2024-06-18 20:31:07 +0100

    Edit school header to show the state of bookmarked school

--- src/app/school/components/SchoolHeader/SchoolHeader.tsx
+++ src/app/school/components/SchoolHeader/SchoolHeader.tsx
@@ -1,10 +1,11 @@
 "use client"
 import { useLocalStorage } from '@rehooks/local-storage';
 import { nanoid } from 'nanoid'
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 
 import { Button, Chip, SchoolLogo, ShareDialog,Toast, Typography } from '~/components'
-import { BookmarkIcon, ShareIcon } from '~/components/Icons'
+import ButtonProps from '~/components/Button/Button.types';
+import { BookmarkIcon, RemoveBookmarkIcon, ShareIcon } from '~/components/Icons'
 
 import styles from './SchoolHeader.module.scss'
 import SchoolHeaderProps from './SchoolHeader.types'
@@ -15,8 +16,52 @@ const SchoolHeader = ({
   logoKey,
   name,
 }: SchoolHeaderProps) => {
+  const unsaveBtnRef = useRef<HTMLAnchorElement | HTMLButtonElement>(null)
+  const saveBtnRef = useRef<HTMLAnchorElement | HTMLButtonElement>(null)
+
   const [bookmarks, setBookmarks]  = useLocalStorage("bookmarks", "")
   const [open, setOpen] = useState(false)
+  const [bookmarkState, setBookmarkState] = useState<'Loading' | 'Bookmark' | 'Saved'>('Loading')
+
+  useEffect(() => {
+    const bookmarksArray = bookmarks.split(',')
+    if(bookmarksArray.includes(id)) {
+      setBookmarkState('Saved');
+    } else {
+      setBookmarkState('Bookmark');
+    }
+  }, [bookmarks, id])
+
+  const handleBookmarkClick = () => {
+    const currentBookmarks = bookmarks.split(",")
+    if(bookmarkState === 'Saved') {
+      const newBookmarks = currentBookmarks.filter(item => item !== id).join(',')
+      setBookmarks(newBookmarks)
+    } else {
+      if (!currentBookmarks.includes(id)) {
+        const newBookmarks = bookmarks ? `${bookmarks},${id}` : id
+        setBookmarks(newBookmarks)
+        setOpen(true)
+      }    
+    }
+  }
+
+  const getBookmarkButtonProps = () => {
+    let props: ButtonProps = {
+      variant: 'tertiary',
+      children: (<><BookmarkIcon /> {bookmarkState}</>),
+      onClick: handleBookmarkClick,
+    }
+
+    if(bookmarkState === 'Loading') {
+      props = {
+        ...props,
+        loading: true,
+      }
+    }
+
+    return props
+  }
 
   return (
     <Fragment>
@@ -53,20 +98,63 @@ const SchoolHeader = ({
 
         <div className={styles['actions-wrapper']}>
           <div className={styles.actions}>
-            <Button 
-              variant='tertiary'
-              onClick={() => {
-                const currentBookmarks = bookmarks.split(",")
-                if (!currentBookmarks.includes(id)) {
-                  const newBookmarks = bookmarks !== "" ? bookmarks + "," + id : id
-                  setBookmarks(newBookmarks)
-                  setOpen(true)
-                }
-              }}
-            >
-              <BookmarkIcon />
-              Bookmark
-            </Button>
+              {
+                bookmarkState === "Saved" ? (
+                  <div 
+                    onMouseEnter={() => {
+                      if (
+                        unsaveBtnRef && unsaveBtnRef?.current
+                        && saveBtnRef && saveBtnRef?.current
+                        && unsaveBtnRef.current.style.display === 'none'
+                        && saveBtnRef.current.style.display === 'flex'
+                      ) {
+                        unsaveBtnRef.current.style.display = 'flex';
+                        saveBtnRef.current.style.display = 'none';
+                      }
+                    }}
+                    onMouseLeave={() => {
+                      if (
+                        unsaveBtnRef && unsaveBtnRef?.current
+                        && saveBtnRef && saveBtnRef?.current
+                        && unsaveBtnRef.current.style.display === 'flex'
+                        && saveBtnRef.current.style.display === 'none'
+                      ) {
+                        unsaveBtnRef.current.style.display = 'none';
+                        saveBtnRef.current.style.display = 'flex';
+                      }
+                    }}
+                  >
+                    <Button
+                      style={{
+                        display: 'flex'
+                      }}
+                      ref={saveBtnRef}
+                      variant='success'
+                    >
+                      <BookmarkIcon />
+                      Saved
+                    </Button>
+                    <Button
+                      style={{
+                        display: 'none'
+                      }}
+                      ref={unsaveBtnRef}
+                      variant='error'
+                      onClick={handleBookmarkClick}
+                    >
+                      <RemoveBookmarkIcon />
+                      Remove
+                    </Button>
+                  </div>
+                ) : (
+                  <Button
+                    {...getBookmarkButtonProps()}
+                  >
+                    <BookmarkIcon />
+                    Bookmark
+                  </Button>
+                )
+              }
             <ShareDialog 
               trigger={
                 <Button variant='secondary'>
@@ -74,8 +162,7 @@ const SchoolHeader = ({
                   Share
                 </Button>
               }
-            />
-            
+            /> 
           </div>
         </div>
       </div>
